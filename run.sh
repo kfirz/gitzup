32c12e3798e2cda4944e570ef73ea02f5cd146a6
Eric Kfir <eric@kfirs.com>
2018-08-26 20:59:01 +0300

    Add watching & rebuild to "run.sh" script

--- run.sh
+++ run.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
 
-docker-compose --file=./deployments/docker-compose.yml \
-               --project-name=gitzup \
-               --project-directory=$(pwd) \
-               up \
-               --quiet-pull \
-               --build \
-               --abort-on-container-exit \
-               --remove-orphans
+function start {
+    docker-compose --file=./deployments/docker-compose.yml \
+                   --project-name=gitzup \
+                   --project-directory=$(pwd) \
+                   up \
+                   --detach \
+                   --quiet-pull \
+                   --build \
+                   --remove-orphans
+    [[ "$?" != "0" ]] && echo "Failed starting compose" && exit 1
+}
+
+function stop {
+    docker-compose --file=./deployments/docker-compose.yml \
+                   --project-name=gitzup \
+                   --project-directory=$(pwd) \
+                   down
+}
+
+function restart {
+    stop
+    start
+}
+
+trap stop EXIT
+
+INOTIFY=$(which inotifywait 2>/dev/null)
+[[ -z "${INOTIFY}" ]] && echo "Install inotify-tools first." >&2 && exit 1
+
+start
+
+echo "Now watching $(pwd)"
+while true; do
+    EVENT=$(inotifywait -e create,modify,delete -r -q ./api ./build ./cmd ./deployments ./internal ./web ./Gopkg.*)
+    echo ""
+    echo "====[ $(date) ]======================================================"
+    echo "Change detected: ${EVENT}"
+    echo "====================================================================="
+    echo ""
+    restart
+done
