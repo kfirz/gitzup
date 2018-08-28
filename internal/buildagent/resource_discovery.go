1d8e52b1c1649e1c58f6e4d939ab423c2c3d32ba
Eric Kfir <eric@kfirs.com>
2018-08-28 13:35:53 +0300

    Fix log statement in discovery skeleton method

--- internal/buildagent/resource_discovery.go
+++ internal/buildagent/resource_discovery.go
@@ -3,6 +3,6 @@ package buildagent
 import "log"
 
 func (resource *Resource) DiscoverState() error {
-	log.Printf("Discovering state for resource '%s'...", resource)
+	log.Printf("Discovering state for resource '%s'...\n", resource.Name)
 	return nil
 }
