82df762a68bbf0bc2c303274507f607a603e6e33
Eric Kfir <eric@kfirs.com>
2018-11-24 21:48:49 +0200

    Push Docker images to GCR on PR/commit pushes
    This will enable pushing interim commit-SHA based Docker images to the
    Gitzup GCR with the commit SHA as the Docker image tag.

--- .travis.build.sh
+++ .travis.build.sh
@@ -3,14 +3,12 @@
 set -exu -o pipefail
 
 if [[ -n "${TRAVIS_TAG}" ]]; then
-    # TODO: if this tag is the GitHub Latest Release, use "make latest"
-    PUSH=true TAG=${TRAVIS_TAG} make docker
+    # TODO: only run "latest" if this tag is the latest GitHub release tag (use "docker" otherwise since we're building an older one)
+    PUSH=true TAG=${TRAVIS_TAG} make latest
 
 elif [[ -n "${TRAVIS_PULL_REQUEST_SHA}" ]]; then
-    # TODO: consider pushing to GCR commit SHA images
-    PUSH=false TAG=${TRAVIS_PULL_REQUEST_SHA} make docker
+    PUSH=true REPO=gcr.io/gitzup TAG=${TRAVIS_PULL_REQUEST_SHA} make docker
 
 else
-    # TODO: consider pushing to GCR commit SHA images
-    PUSH=false TAG=${TRAVIS_COMMIT} make docker
+    PUSH=true REPO=gcr.io/gitzup TAG=${TRAVIS_COMMIT} make docker
 fi
